e7c3816553ce3fbf3c3b22f6c636993f7a88cc35
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-04-12 16:27:03 -0300

    initial commit

---
+++ react/trabalhando-com-rotas/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bem vindo a pagina home</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ react/trabalhando-com-rotas/src/pages/Sobre/index.js
@@ -0,0 +1,9 @@
+function Sobre() {
+  return (
+    <div>
+      <h1>Pagina sobre a empresa</h1>
+    </div>
+  );
+}
+
+export default Sobre;
